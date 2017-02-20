4c7e231d6a296a52847c0c08e66e9f10d3d079aa
Tim Carstens <intoverflow@gmail.com>
2017-02-20 15:48:05 -0800

    Finished definition of Monoidal.

--- qp/Cat/Monoidal.lean
+++ qp/Cat/Monoidal.lean
@@ -25,9 +25,20 @@ structure Monoidal (C : Cat.{ℓobj ℓhom})
    (right_unitor_inv : Fun.id C ↣↣ Fun.right_fill tensor unit)
    (right_unitor_iso : NatIso right_unitor right_unitor_inv)
    (triangle : ∀ {x y : [[C]]}
-               , let lefty : Fun.right_comp tensor tensor ⟨x, unit, y⟩ →→ tensor ⟨x, y⟩
+               , let lefty : tensor ⟨x, tensor ⟨unit, y⟩ ⟩ →→ tensor ⟨x, y⟩
                       := tensor^.hom ⟨ ⟨⟨x⟩⟩, left_unitor y ⟩
                  in lefty ∘∘ assoc_right ⟨x, unit, y⟩ = tensor^.hom ⟨ right_unitor x, ⟨⟨y⟩⟩ ⟩)
-   -- TODO: pentagon equality
+   (pentagon : ∀ {w x y z : [[C]]}
+               , let x₁ : tensor ⟨tensor ⟨tensor ⟨w, x⟩ , y⟩ , z⟩ →→ tensor ⟨tensor ⟨w, tensor ⟨x, y⟩ ⟩, z⟩
+                      := tensor^.hom ⟨ assoc_right ⟨w, x, y⟩, ⟨⟨z⟩⟩ ⟩ in
+                 let x₂ : tensor ⟨tensor ⟨w, tensor ⟨x, y⟩ ⟩, z⟩ →→ tensor ⟨w, tensor ⟨tensor ⟨x, y⟩, z⟩ ⟩
+                      := assoc_right ⟨w, tensor ⟨x, y⟩, z⟩ in
+                 let x₃ : tensor ⟨w, tensor ⟨tensor ⟨x, y⟩, z⟩ ⟩ →→ tensor ⟨w, tensor ⟨x, tensor ⟨y, z⟩ ⟩ ⟩
+                      := tensor^.hom ⟨ ⟨⟨w⟩⟩, assoc_right ⟨x, y, z⟩ ⟩ in
+                 let y₁ : tensor ⟨tensor ⟨tensor ⟨w, x⟩ , y⟩ , z⟩ →→ tensor ⟨tensor ⟨w, x⟩, tensor ⟨y, z⟩ ⟩
+                      := assoc_right ⟨tensor ⟨w, x⟩, y, z⟩ in
+                 let y₂ : tensor ⟨tensor ⟨w, x⟩, tensor ⟨y, z⟩ ⟩ →→ tensor ⟨w, tensor ⟨x, tensor ⟨y, z⟩ ⟩ ⟩
+                      := assoc_right ⟨w, x, tensor ⟨y, z⟩ ⟩
+                 in x₃ ∘∘ x₂ ∘∘ x₁ = y₂ ∘∘ y₁)
 
 end qp
