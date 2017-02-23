4b0313f46ee2e8e3664300ee73b497bc94544e47
Tim Carstens <intoverflow@gmail.com>
2017-02-23 14:33:27 -0800

    Braided monoidal category

--- qp/Cat/Monoidal.lean
+++ qp/Cat/Monoidal.lean
@@ -27,20 +27,57 @@ structure Monoidal (C : Cat.{ℓobj ℓhom})
    (right_unitor_iso : NatIso right_unitor right_unitor_inv)
    (triangle : ∀ {x y : [[C]]}
                , let lefty : tensor ⟨x, tensor ⟨unit, y⟩ ⟩ →→ tensor ⟨x, y⟩
-                      := tensor^.hom ⟨ ⟨⟨x⟩⟩, left_unitor y ⟩
+                          := tensor^.hom ⟨ ⟨⟨x⟩⟩, left_unitor y ⟩
                  in lefty ∘∘ assoc_right ⟨x, unit, y⟩ = tensor^.hom ⟨ right_unitor x, ⟨⟨y⟩⟩ ⟩)
    (pentagon : ∀ {w x y z : [[C]]}
                , let x₁ : tensor ⟨tensor ⟨tensor ⟨w, x⟩ , y⟩ , z⟩ →→ tensor ⟨tensor ⟨w, tensor ⟨x, y⟩ ⟩, z⟩
-                      := tensor^.hom ⟨ assoc_right ⟨w, x, y⟩, ⟨⟨z⟩⟩ ⟩ in
+                       := tensor^.hom ⟨ assoc_right ⟨w, x, y⟩, ⟨⟨z⟩⟩ ⟩ in
                  let x₂ : tensor ⟨tensor ⟨w, tensor ⟨x, y⟩ ⟩, z⟩ →→ tensor ⟨w, tensor ⟨tensor ⟨x, y⟩, z⟩ ⟩
-                      := assoc_right ⟨w, tensor ⟨x, y⟩, z⟩ in
+                       := assoc_right ⟨w, tensor ⟨x, y⟩, z⟩ in
                  let x₃ : tensor ⟨w, tensor ⟨tensor ⟨x, y⟩, z⟩ ⟩ →→ tensor ⟨w, tensor ⟨x, tensor ⟨y, z⟩ ⟩ ⟩
-                      := tensor^.hom ⟨ ⟨⟨w⟩⟩, assoc_right ⟨x, y, z⟩ ⟩ in
+                       := tensor^.hom ⟨ ⟨⟨w⟩⟩, assoc_right ⟨x, y, z⟩ ⟩ in
                  let y₁ : tensor ⟨tensor ⟨tensor ⟨w, x⟩ , y⟩ , z⟩ →→ tensor ⟨tensor ⟨w, x⟩, tensor ⟨y, z⟩ ⟩
-                      := assoc_right ⟨tensor ⟨w, x⟩, y, z⟩ in
+                       := assoc_right ⟨tensor ⟨w, x⟩, y, z⟩ in
                  let y₂ : tensor ⟨tensor ⟨w, x⟩, tensor ⟨y, z⟩ ⟩ →→ tensor ⟨w, tensor ⟨x, tensor ⟨y, z⟩ ⟩ ⟩
-                      := assoc_right ⟨w, x, tensor ⟨y, z⟩ ⟩
+                       := assoc_right ⟨w, x, tensor ⟨y, z⟩ ⟩
                  in x₃ ∘∘ x₂ ∘∘ x₁ = y₂ ∘∘ y₁)
+
+-- A braided monoidal category.
+structure Braided {C : Cat.{ℓobj ℓhom}} (C_Monoidal : Monoidal C)
+    : Type ((max ℓobj ℓhom) + 1)
+:= (braid : C_Monoidal^.tensor ↣↣ Fun.flip C_Monoidal^.tensor)
+   (unbraid : Fun.flip C_Monoidal^.tensor ↣↣ C_Monoidal^.tensor)
+   (braid_unbraid_iso : NatIso unbraid braid)
+   (hex_left
+     : ∀ {x y z : [[C]]}
+       , let x₁ : C_Monoidal^.tensor ⟨C_Monoidal^.tensor ⟨x, y⟩ , z⟩ →→ C_Monoidal^.tensor ⟨x, C_Monoidal^.tensor ⟨y, z⟩ ⟩
+               := C_Monoidal^.assoc_right ⟨x, y, z⟩ in
+         let x₂ : C_Monoidal^.tensor ⟨x, C_Monoidal^.tensor ⟨y, z⟩ ⟩ →→ C_Monoidal^.tensor ⟨C_Monoidal^.tensor ⟨y, z⟩ , x⟩
+               := braid ⟨x, C_Monoidal^.tensor ⟨y, z⟩ ⟩ in
+         let x₃ : C_Monoidal^.tensor ⟨C_Monoidal^.tensor ⟨y, z⟩ , x⟩ →→ C_Monoidal^.tensor ⟨y, C_Monoidal^.tensor ⟨z, x⟩ ⟩
+               := C_Monoidal^.assoc_right ⟨y, z, x⟩ in
+         let y₁ : C_Monoidal^.tensor ⟨C_Monoidal^.tensor ⟨x, y⟩ , z⟩ →→ C_Monoidal^.tensor ⟨C_Monoidal^.tensor ⟨y, x⟩ , z⟩
+               := C_Monoidal^.tensor^.hom ⟨ braid ⟨x, y⟩, ⟨⟨z⟩⟩ ⟩ in
+         let y₂ : C_Monoidal^.tensor ⟨C_Monoidal^.tensor ⟨y, x⟩ , z⟩ →→ C_Monoidal^.tensor ⟨y, C_Monoidal^.tensor ⟨x, z⟩ ⟩
+               := C_Monoidal^.assoc_right ⟨y, x, z⟩ in
+         let y₃ : C_Monoidal^.tensor ⟨y, C_Monoidal^.tensor ⟨x, z⟩ ⟩ →→ C_Monoidal^.tensor ⟨y, C_Monoidal^.tensor ⟨z, x⟩ ⟩
+               := C_Monoidal^.tensor^.hom ⟨ ⟨⟨y⟩⟩, braid ⟨x, z⟩ ⟩
+         in y₃ ∘∘ y₂ ∘∘ y₁ = x₃ ∘∘ x₂ ∘∘ x₁)
+    (hex_right
+      : ∀ {x y z : [[C]]}
+        , let x₁ : C_Monoidal^.tensor ⟨x, C_Monoidal^.tensor ⟨y, z⟩ ⟩ →→ C_Monoidal^.tensor ⟨C_Monoidal^.tensor ⟨x, y⟩ , z⟩
+                := C_Monoidal^.assoc_left ⟨x, y, z⟩ in
+          let x₂ : C_Monoidal^.tensor ⟨C_Monoidal^.tensor ⟨x, y⟩ , z⟩ →→ C_Monoidal^.tensor ⟨z, C_Monoidal^.tensor ⟨x, y⟩ ⟩
+                := braid ⟨C_Monoidal^.tensor ⟨x, y⟩, z⟩ in
+          let x₃ : C_Monoidal^.tensor ⟨z, C_Monoidal^.tensor ⟨x, y⟩ ⟩ →→ C_Monoidal^.tensor ⟨C_Monoidal^.tensor ⟨z, x⟩ , y⟩
+                := C_Monoidal^.assoc_left ⟨z, x, y⟩ in
+          let y₁ : C_Monoidal^.tensor ⟨x, C_Monoidal^.tensor ⟨y, z⟩ ⟩ →→ C_Monoidal^.tensor ⟨x, C_Monoidal^.tensor ⟨z, y⟩ ⟩
+                := C_Monoidal^.tensor^.hom ⟨ ⟨⟨x⟩⟩, braid ⟨y, z⟩ ⟩ in
+          let y₂ : C_Monoidal^.tensor ⟨x, C_Monoidal^.tensor ⟨z, y⟩ ⟩ →→ C_Monoidal^.tensor ⟨C_Monoidal^.tensor ⟨x, z⟩ , y⟩
+                := C_Monoidal^.assoc_left ⟨x, z, y⟩ in
+          let y₃ : C_Monoidal^.tensor ⟨C_Monoidal^.tensor ⟨x, z⟩ , y⟩ →→ C_Monoidal^.tensor ⟨C_Monoidal^.tensor ⟨z, x⟩ , y⟩
+                := C_Monoidal^.tensor^.hom ⟨ braid ⟨x, z⟩, ⟨⟨y⟩⟩ ⟩
+          in y₃ ∘∘ y₂ ∘∘ y₁ = x₃ ∘∘ x₂ ∘∘ x₁)
 
 /-! #brief A cartesian monoidal category.
 -/
