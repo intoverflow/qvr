045e483efb26eee9e34efb8f5ad2fddde223d79e
Tim Carstens <intoverflow@gmail.com>
2017-02-27 17:58:31 -0800

    Dependent products in LeanCat

--- qp/LeanCat/DepProduct.lean
+++ qp/LeanCat/DepProduct.lean
@@ -13,34 +13,89 @@ namespace qp
 Dependent products.
 ---------------------------------------------------------------------------- -/
 
-/-
+/-! #brief The dependent product functor.
+-/
+@[reducible] definition {ℓ} LeanCat.DepProdFun
+    {T₀ T₁ : [[LeanCat.{ℓ}]]} (base : T₀ → T₁)
+    : SliceCat LeanCat T₀ ⇉⇉ SliceCat LeanCat T₁
+:= { obj
+      := λ X
+         , { dom := Σ (t₁ : T₁), ∀ (t₀ : {t₀ : T₀ // base t₀ = t₁})
+                                 , { x : X^.dom // X^.hom x = t₀^.elt_of}
+           , hom := λ σ, σ^.fst
+           }
+   , hom
+      := λ X Y f
+         , { hom := λ σ, { fst := σ^.fst
+                         , snd := λ t₀, { elt_of := f^.hom (σ^.snd t₀)
+                                        , has_property := begin
+                                                            apply eq.trans (congr_fun (eq.symm f^.triangle) (σ^.snd t₀)),
+                                                            apply (σ^.snd t₀)^.has_property
+                                                          end
+                                        }
+                         }
+           , triangle := rfl
+           }
+   , hom_id
+      := λ X
+         , begin
+             apply SliceCat.Hom.eq,
+             apply funext, intro σ,
+             cases σ with t₁ σ,
+             apply congr_arg (sigma.mk t₁),
+             apply funext, intro t₀,
+             apply subtype.eq,
+             apply rfl
+           end
+   , hom_circ
+      := λ X Y Z g f
+         , begin
+             apply SliceCat.Hom.eq,
+             apply funext, intro σ,
+             cases σ with t₁ σ,
+             simp,
+             apply congr_arg (sigma.mk t₁),
+             apply funext, intro t₀,
+             apply subtype.eq,
+             apply rfl
+           end
+   }
+
 /-! #brief The Lean categories have dependent products.
 -/
 @[reducible] definition {ℓ} LeanCat.HasAllDepProducts
     : HasAllDepProducts @LeanCat.HasAllPullbacks.{ℓ}
 := λ T₀ T₁ base
-   , { dprod := { obj := λ x, { dom := {σ : LeanCat^.hom T₀ x^.dom // LeanCat^.circ x^.hom σ = LeanCat^.id T₀ }
-                              , hom := λ σ, begin end -- λ σ, σ^.elt_of^.snd
-                              }
-                , hom := begin end
-                , hom_id := begin end
-                , hom_circ := begin end
-                }
+   , { dprod := LeanCat.DepProdFun base
      , adj
-        := { unit := { component := λ x, { hom := λ x₀, { elt_of := λ t₀, { elt_of := { fst := t₀, snd := x₀ }, has_property := begin end }
-                                                        , has_property := rfl
-                                                        }
-                                         , triangle := begin end
+        := { unit := { component := λ X, { hom := λ x, { fst := X^.hom x
+                                                       , snd := λ t₀, { elt_of := { elt_of := { fst := t₀^.elt_of, snd := x }
+                                                                                  , has_property := t₀^.has_property
+                                                                                  }
+                                                                      , has_property := rfl
+                                                                      }
+                                                       }               
+                                         , triangle := begin apply funext, intro x, apply rfl end
                                          }
-                     , transport := begin end
+                     , transport := λ X Y f, begin apply SliceCat.Hom.eq, apply funext, intro x, dsimp /- true because of f^.triangle -/ exact sorry end
                      }
-           , counit := { component := begin end
-                       , transport := begin end
+           , counit := { component := λ X, { hom := λ t₀t₁σ, let t₀ := t₀t₁σ^.elt_of^.fst in
+                                                             let ωt₀ := t₀t₁σ^.has_property in
+                                                             let σ := t₀t₁σ^.elt_of^.snd^.snd
+                                                             in (σ { elt_of := t₀, has_property := ωt₀ })^.elt_of
+                                           , triangle := begin apply funext, intro t₀t₁σ,
+                                                           cases t₀t₁σ with t₀t₁σ ωt₀,
+                                                           cases t₀t₁σ with t₀ t₁σ,
+                                                           cases t₁σ with t₁ σ,
+                                                           apply eq.symm,
+                                                           apply (σ {elt_of := t₀, has_property := ωt₀})^.has_property
+                                                         end
+                                           }
+                       , transport := λ X Y f, begin apply SliceCat.Hom.eq, apply funext, intro x, dsimp, apply rfl end
                        }
-           , id_left := begin end
-           , id_right := begin end
+           , id_left := sorry
+           , id_right := sorry
            }
      }
--/
 
 end qp
